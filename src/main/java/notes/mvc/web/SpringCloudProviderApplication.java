da22dde833ed9f652fa51c73ffcf1fa5b1846d79
zhouzhongyi <zzy520com@qq.com>
2020-01-11 17:45:15 +0800

    --

--- src/main/java/notes/mvc/web/SpringCloudProviderApplication.java
+++ src/main/java/notes/mvc/web/SpringCloudProviderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+//@EnableDiscoveryClient	//更通用，包含EnableEurekaClient功能
 @SpringBootApplication
 public class SpringCloudProviderApplication {
 
